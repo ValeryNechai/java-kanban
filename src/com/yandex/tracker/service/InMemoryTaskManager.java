9720921da4c45c20a799bda4346d6b8ef7fd3911
Valery Nechai <lerkapolonnikova@yandex.ru>
2025-09-07 23:38:46 +0300

    Исправить замечания 8 спринт

--- src/com/yandex/tracker/service/InMemoryTaskManager.java
+++ src/com/yandex/tracker/service/InMemoryTaskManager.java
@@ -5,7 +5,6 @@ import com.yandex.tracker.model.Subtask;
 import com.yandex.tracker.model.Task;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
